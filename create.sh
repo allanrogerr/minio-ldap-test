c0200dce3c9e8ca1f16282ee234a28baa8c4cec9
Allan Reid <allanrogerreid@gmail.com>
2024-12-19 14:17:21 -0800

    Initial

--- create.sh
+++ create.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+sudo snap install yq
+
 docker compose down
 docker compose up --build -d
 sleep 3
@@ -8,8 +11,8 @@ docker compose restart minio
 sleep 3
 
 echo "Show all users and groups in the LDAP directory"
-docker exec ldap-openldap-1 ldapsearch -H ldap://localhost -D "cn=admin,dc=example,dc=org" -w "admin123" -b "dc=example,dc=org" -x "(objectclass=inetOrgPerson)"
-docker exec ldap-openldap-1 ldapsearch -H ldap://localhost -D "cn=admin,dc=example,dc=org" -w "admin123" -b "dc=example,dc=org" -x "(objectclass=groupOfNames)"
+docker exec minio-ldap-test-openldap-1 ldapsearch -H ldap://localhost -D "cn=admin,dc=example,dc=org" -w "admin123" -b "dc=example,dc=org" -x "(objectclass=inetOrgPerson)"
+docker exec minio-ldap-test-openldap-1 ldapsearch -H ldap://localhost -D "cn=admin,dc=example,dc=org" -w "admin123" -b "dc=example,dc=org" -x "(objectclass=groupOfNames)"
 
 echo "Create versioned ogmatic-zoo bucket"
 mc alias set local http://localhost:9000 minio minio123
@@ -23,15 +26,19 @@ mc idp ldap policy attach local readonly --group='cn=admins,ou=groups,dc=example
 mc idp ldap policy attach local ogmatic-zoo-policy --group='cn=admins,ou=groups,dc=example,dc=org'
 sleep 1
 echo "You can log on using user1/pwd1 or user2/pwd2"
-#mc idp ldap policy entities local --user='CN=admins,ou=groups,dc=example,dc=org'
+#mc idp ldap policy entities local --user='cn=user1,ou=users,dc=example,dc=org'
 
-USER1_CREDS_JSON=$(curl -X POST 'http://localhost:9000?Action=AssumeRoleWithLDAPIdentity&LDAPUsername=user1&LDAPPassword=pwd1&Version=2011-06-15' | xq | jq .AssumeRoleWithLDAPIdentityResponse.AssumeRoleWithLDAPIdentityResult.Credentials)
+USER1_CREDS_JSON=$(curl -X POST 'http://localhost:9000?Action=AssumeRoleWithLDAPIdentity&LDAPUsername=user1&LDAPPassword=pwd1&Version=2011-06-15' | yq -p xml -o=json .AssumeRoleWithLDAPIdentityResponse.AssumeRoleWithLDAPIdentityResult.Credentials)
 export MC_HOST_user1=http://$(echo $USER1_CREDS_JSON | jq -r .AccessKeyId):$(echo $USER1_CREDS_JSON | jq -r .SecretAccessKey):$(echo $USER1_CREDS_JSON | jq -r .SessionToken)@localhost:9000
 
-USER2_CREDS_JSON=$(curl -X POST 'http://localhost:9000?Action=AssumeRoleWithLDAPIdentity&LDAPUsername=user2&LDAPPassword=pwd2&Version=2011-06-15' | xq | jq .AssumeRoleWithLDAPIdentityResponse.AssumeRoleWithLDAPIdentityResult.Credentials)
+USER2_CREDS_JSON=$(curl -X POST 'http://localhost:9000?Action=AssumeRoleWithLDAPIdentity&LDAPUsername=user2&LDAPPassword=pwd2&Version=2011-06-15' | yq -p xml -o=json .AssumeRoleWithLDAPIdentityResponse.AssumeRoleWithLDAPIdentityResult.Credentials)
 export MC_HOST_user2=http://$(echo $USER2_CREDS_JSON | jq -r .AccessKeyId):$(echo $USER2_CREDS_JSON | jq -r .SecretAccessKey):$(echo $USER2_CREDS_JSON | jq -r .SessionToken)@localhost:9000
 
+echo $MC_HOST_user1
+echo $MC_HOST_user2
+
 echo "Copying using user 1 (should succeed)"
+#sleep 10
 mc idp ldap policy entities local --user='cn=user1,ou=users,dc=example,dc=org'
 mc cp create.sh user1/ogmatic-zoo/cores_er/schemas/Cores-PD.Design.DEF_Instance.json
 mc cp docker-compose.yaml user1/ogmatic-zoo/cores_er/schemas/Cores-PD.Design.DEF_Instance.json
